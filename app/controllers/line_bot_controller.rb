87f061dbeb14dd2835790193d29bcae9dfb603e4
FUMIGear <iso.fumi429@gmail.com>
2023-05-06 10:06:17 +0900

    Spt1を解いたので、模範回答を見る前に一度コミットしました。 ・一覧表示、最初のタスク削除、タスクの全削除機能を実装しました。 ・プライベートメソッドにifmessageを作りました。

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -22,33 +22,42 @@ class LineBotController < ApplicationController
         # LINE からテキストが送信されたときの処理を記述する
         # Task10で追記した
         message = event["message"]["text"]
+        message = message.chomp #不要な改行をなくすメソッド（複数個に弱い）
         # binding.pry
         # 送信されたメッセージをデータベースに保存するコードを書こう
-        # @task = Task.new(title: message)
-        Task.create!(title: message)
-        # binding.pry #ここまで進んだ。saveメソッドの作り込み
-        # puts Task.pluck(:title) #.select(:title)よりこっちの方がいいかも
-        # respond_to do |format|
-          # if @task.save
-            # format.html { redirect_to task_url(@task), notice: "Task was successfully created." }
-            # format.json { render :show, status: :created, location: @task }
-          # else
-            # format.html { render :new, status: :unprocessable_entity }
-            # format.json { render json: @task.errors, status: :unprocessable_entity }
-          # end
+        ifmessage(message)
+        binding.pry #インスタンス変数でないとなぜかリターンがない。
+        # case message
+        #   when "一覧表示"
+        #     # 一覧表示はできるが、見えにくい。
+        #     reply_message = {
+        #       type: "text", # 入力したメッセージ（上のtext変数）／これがないとエラーになる
+        #       text: "【今までに登録したタスク】\n#{Task.pluck(:title).join("\n")}"
+        #     }
+        #     # return reply_message
+        #   when "削除"
+        #     # これだと最初のタスクしか消せない。
+        #     text = Task.first.title #削除されるタスクのタイトル
+        #     Task.first.destroy #タスクの削除
+        #     reply_message = {
+        #       type: "text", # 入力したメッセージ（上のtext変数）
+        #       text: "bomb!\n\"#{text}\"\nは削除されました！！"
+        #     }
+        #     # return reply_message
+        #   else
+        #     Task.create!(title: message)
+        #     reply_message = {
+        #       type: "text", # 入力したメッセージ（上のtext変数）
+        #       text: "ぽーーん！\n\"#{message}\"\nが登録されました！！"
+        #       # \n今まで登録されたタスクは・・・\n#{Task.pluck(:title)}\nです。"
+        #     }
         # end
-        # ここまでコード
-        # LINEに返すメッセージを考えてみよう。""は改行しても繋がってる。
-        reply_message = {
-          type: "text", # 入力したメッセージ（上のtext変数）
-          text: "ぽーーん！\n\"#{message}\"\nが登録されました！！"
-          # \n今まで登録されたタスクは・・・\n#{Task.pluck(:title)}\nです。"
-		    }
-        client.reply_message(event["replyToken"], reply_message)
+
+        # client.reply_message(event["replyToken"], reply_message)
+        client.reply_message(event["replyToken"], @reply_message)
         # ここまでTask10で追記
       end
     end
-
     # LINE の webhook API との連携をするために status code 200 を返す
     render json: { status: :ok }
   end
@@ -61,22 +70,41 @@ class LineBotController < ApplicationController
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     end
   end
-# end
 
-  # Task6で作ったコード（callbackメソッドの中身は作る必要なかった）
-  # def callback
+  def ifmessage(message)
     # binding.pry
-    # @task = Task.new(title: params[:title])
-    # # binding.pry
-    # respond_to do |format|
-    #   if @task.save
-    #     # format.html { redirect_to task_url(@task), notice: "Task was successfully created." }
-    #     format.json { render :show, status: :created, location: @task }
-    #   else
-    #     # format.html { render :new, status: :unprocessable_entity }
-    #     format.json { render json: @task.errors, status: :unprocessable_entity }
-    #   end
-    # end
-  # end
-
+    case message
+      when "一覧表示"
+        # 一覧表示はできるが、見えにくい。
+        @reply_message = {
+          type: "text", # 入力したメッセージ（上のtext変数）／これがないとエラーになる
+          text: "【今までに登録したタスク】\n#{Task.pluck(:title).join("\n")}"
+        }
+      when "削除"
+        # これだと最初のタスクしか消せない。
+        text = Task.first.title #削除されるタスクのタイトル
+        Task.first.destroy #タスクの削除
+        @reply_message = {
+          type: "text", # 入力したメッセージ（上のtext変数）
+          text: "bomb!\n\"#{text}\"\nは削除されました！！"
+        }
+      when "全削除"
+        Task.destroy_all #タスクの削除
+        @reply_message = {
+          type: "text", # 入力したメッセージ（上のtext変数）
+          text: "bomb!\n全てのタスクは削除されました！"
+        }
+      else
+        Task.create!(title: message)
+        @reply_message = {
+          type: "text", # 入力したメッセージ（上のtext変数）
+          text: "ぽーーん！\n\"#{message}\"\nが登録されました！！"
+          # \n今まで登録されたタスクは・・・\n#{Task.pluck(:title)}\nです。"
+        }
+    end
+    # binding.pry
+    # return reply_message # returnが機能してない。インスタンス変数にしたらうまくいった。
+    # return testmessage = reply_message
+    # client.reply_message(event["replyToken"], reply_message) # 使ってる変数が多い。
+  end
 end
