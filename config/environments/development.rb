3c275443b7abeec83a01164d4453013260a676bd
FUMIGear <iso.fumi429@gmail.com>
2023-12-12 18:22:48 +0900

    修正しました。

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,8 +75,10 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   # config.hosts = ["fe79de82f878.ngrok.io", "localhost"] #Task10で追加
   # config.hosts = ["e7d5-211-16-48-241.ngrok-free.app", "localhost"] #自分のURLに修正した。
-  # config.hosts << "e7d5-211-16-48-241.ngrok-free.app" #書き方が変わってる。
-  # config.hosts << ["e7d5-211-16-48-241.ngrok-free.app", "localhost"]  #こっちなら、ホスト名を指定できる。
-  config.hosts << "line-todo-list.onrender.com"  #Render.com用
+  # config.hosts << "e7d5-211-16-48-241.ngrok-free.app" # 書き方が変わってる。
+  # config.hosts << ["e7d5-211-16-48-241.ngrok-free.app", "localhost"]  #これなら、ホスト名を指定できる。
+  # config.hosts << "line-todo-list.onrender.com"  #Render.com用
+  config.hosts.clear
+
   # config.hosts = ["127.0.0.1", "localhost"] #ローカルは無理！
 end
